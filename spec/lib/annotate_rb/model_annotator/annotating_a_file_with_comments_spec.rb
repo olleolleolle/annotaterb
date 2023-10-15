c66cd2f88de07dcae9ae761e57a747e748ae8acb
Andrew W. Lee <git@drewlee.com>
2023-10-15 16:13:58 -0700

    Add cases for annotating a fresh file

---
+++ spec/lib/annotate_rb/model_annotator/annotating_a_file_with_comments_spec.rb
@@ -0,0 +1,266 @@
+# frozen_string_literal: true
+
+RSpec.describe AnnotateRb::ModelAnnotator::SingleFileAnnotator do
+  include AnnotateTestHelpers
+  include AnnotateTestConstants
+
+  let(:options) { AnnotateRb::Options.new({}) }
+  let(:schema_info) do
+    <<~SCHEMA
+      # == Schema Information
+      #
+      # Table name: users
+      #
+      #  id                     :bigint           not null, primary key
+      #
+    SCHEMA
+  end
+
+  before do
+    @model_dir = Dir.mktmpdir("annotaterb")
+    (@model_file_name, _file_content) = write_model("user.rb", starting_file_content)
+  end
+
+  context "when annotating a fresh file" do
+    context "with magic comments before class declaration with line break between" do
+      let(:starting_file_content) do
+        <<~FILE
+          # typed: strong
+
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+      let(:expected_file_content) do
+        <<~FILE
+          # typed: strong
+
+          # == Schema Information
+          #
+          # Table name: users
+          #
+          #  id                     :bigint           not null, primary key
+          #
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it "writes the expected annotations to the file" do
+        AnnotateRb::ModelAnnotator::SingleFileAnnotator.call(@model_file_name, schema_info, :position_in_class, options)
+        expect(File.read(@model_file_name)).to eq(expected_file_content)
+      end
+    end
+
+    context "with magic comments before class declaration without a line break between" do
+      let(:starting_file_content) do
+        <<~FILE
+          # typed: strong
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+      let(:expected_file_content) do
+        <<~FILE
+          # typed: strong
+
+          # == Schema Information
+          #
+          # Table name: users
+          #
+          #  id                     :bigint           not null, primary key
+          #
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it "writes the expected annotations to the file" do
+        AnnotateRb::ModelAnnotator::SingleFileAnnotator.call(@model_file_name, schema_info, :position_in_class, options)
+        expect(File.read(@model_file_name)).to eq(expected_file_content)
+      end
+    end
+
+    context "with human comments before class declaration with line break between" do
+      let(:starting_file_content) do
+        <<~FILE
+          # some comment about the class
+
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+      let(:expected_file_content) do
+        <<~FILE
+          # == Schema Information
+          #
+          # Table name: users
+          #
+          #  id                     :bigint           not null, primary key
+          #
+          # some comment about the class
+
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it "writes the expected annotations to the file" do
+        AnnotateRb::ModelAnnotator::SingleFileAnnotator.call(@model_file_name, schema_info, :position_in_class, options)
+        expect(File.read(@model_file_name)).to eq(expected_file_content)
+      end
+    end
+
+    context "with human comments before class declaration without a line break between" do
+      let(:starting_file_content) do
+        <<~FILE
+          # some comment about the class
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+      let(:expected_file_content) do
+        <<~FILE
+          # == Schema Information
+          #
+          # Table name: users
+          #
+          #  id                     :bigint           not null, primary key
+          #
+          # some comment about the class
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it "writes the expected annotations to the file" do
+        AnnotateRb::ModelAnnotator::SingleFileAnnotator.call(@model_file_name, schema_info, :position_in_class, options)
+        expect(File.read(@model_file_name)).to eq(expected_file_content)
+      end
+    end
+
+    context "with `position_in_class: after`" do
+      let(:options) { AnnotateRb::Options.new({position_in_class: :after}) }
+
+      context "with magic comments before class declaration with line break between" do
+        let(:starting_file_content) do
+          <<~FILE
+            # typed: strong
+
+            class User < ApplicationRecord
+            end
+          FILE
+        end
+        let(:expected_file_content) do
+          <<~FILE
+            # typed: strong
+
+            class User < ApplicationRecord
+            end
+
+            # == Schema Information
+            #
+            # Table name: users
+            #
+            #  id                     :bigint           not null, primary key
+            #
+          FILE
+        end
+
+        it "writes the expected annotations to the file" do
+          AnnotateRb::ModelAnnotator::SingleFileAnnotator.call(@model_file_name, schema_info, :position_in_class, options)
+          expect(File.read(@model_file_name)).to eq(expected_file_content)
+        end
+      end
+
+      context "with magic comments before class declaration without a line break between" do
+        let(:starting_file_content) do
+          <<~FILE
+            # typed: strong
+            class User < ApplicationRecord
+            end
+          FILE
+        end
+        let(:expected_file_content) do
+          <<~FILE
+            # typed: strong
+            class User < ApplicationRecord
+            end
+
+            # == Schema Information
+            #
+            # Table name: users
+            #
+            #  id                     :bigint           not null, primary key
+            #
+          FILE
+        end
+
+        it "writes the expected annotations to the file" do
+          AnnotateRb::ModelAnnotator::SingleFileAnnotator.call(@model_file_name, schema_info, :position_in_class, options)
+          expect(File.read(@model_file_name)).to eq(expected_file_content)
+        end
+      end
+
+      context "with human comments before class declaration with line break between" do
+        let(:starting_file_content) do
+          <<~FILE
+            # some comment about the class
+
+            class User < ApplicationRecord
+            end
+          FILE
+        end
+        let(:expected_file_content) do
+          <<~FILE
+            # some comment about the class
+
+            class User < ApplicationRecord
+            end
+
+            # == Schema Information
+            #
+            # Table name: users
+            #
+            #  id                     :bigint           not null, primary key
+            #
+          FILE
+        end
+
+        it "writes the expected annotations to the file" do
+          AnnotateRb::ModelAnnotator::SingleFileAnnotator.call(@model_file_name, schema_info, :position_in_class, options)
+          expect(File.read(@model_file_name)).to eq(expected_file_content)
+        end
+      end
+
+      context "with human comments before class declaration without a line break between" do
+        let(:starting_file_content) do
+          <<~FILE
+            # some comment about the class
+            class User < ApplicationRecord
+            end
+          FILE
+        end
+        let(:expected_file_content) do
+          <<~FILE
+            # some comment about the class
+            class User < ApplicationRecord
+            end
+
+            # == Schema Information
+            #
+            # Table name: users
+            #
+            #  id                     :bigint           not null, primary key
+            #
+          FILE
+        end
+
+        it "writes the expected annotations to the file" do
+          AnnotateRb::ModelAnnotator::SingleFileAnnotator.call(@model_file_name, schema_info, :position_in_class, options)
+          expect(File.read(@model_file_name)).to eq(expected_file_content)
+        end
+      end
+    end
+  end
+end
